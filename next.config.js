0e2dc4a0234c59f329f31ada23e8c0cb5d7ccca0
Minu Mathew <minumpmathews@gmail.com>
2024-05-29 14:24:01 -0500

    flask server

--- next.config.js
+++ next.config.js
@@ -14,4 +14,17 @@ const nextConfig = {
   }
 };
 
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
+      },
+    ]
+  },
+}
+
 module.exports = nextConfig;
+
+
