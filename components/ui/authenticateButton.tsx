2dcf21d363da480712b4616c70089662dc3ea035
Minu Mathew <minumpmathews@gmail.com>
2024-06-03 11:29:49 -0500

    Revert "authentication"
    This reverts commit e467b02810923b45ff65fd3539800cad426b715d.

--- components/ui/authenticateButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@/components/ui/button';
-
-const AuthenticateButton = ( { onClick }: { onClick: () => void }) => {
-  return (
-    <Button onClick={onClick}>Authenticate</Button>
-  );
-};
-
-export default AuthenticateButton;
