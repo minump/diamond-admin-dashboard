b96120b6ee416825972efd41a8c83599b8d7c058
rohan-uiuc <rohan13@illinois.edu>
2024-06-04 14:58:58 -0500

    Adding NextAuth Config for with Custom Provider for Globus Compute

--- api/auth/[...nextauth]/route.ts
+++ api/auth/[...nextauth]/route.ts
@@ -1,1 +1,7 @@
-export { GET, POST } from '@/lib/auth';
+import { NextApiRequest, NextApiResponse } from "next";
+import { getOAuth2UrlAndLogin } from "@/lib/taskHandlers";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const oauthUrl = await getOAuth2UrlAndLogin();
+    res.redirect(oauthUrl);
+}
