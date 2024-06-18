fd7e3430959e816da4d9c7e957a241a85dc40c22
Minu Mathew <minumpmathews@gmail.com>
2024-06-18 14:28:17 -0500

    WIP

--- api/index.py
+++ api/index.py
@@ -1,6 +1,168 @@
-from flask import ( Flask, request, jsonify,  make_response, Request, Response)
+from flask import ( Flask, request, jsonify,  make_response, Request, Response, redirect, url_for, session, flash, render_template)
 from diamond.wrapper.wrapper import register_container
+from globus_sdk import (RefreshTokenAuthorizer, TransferAPIError,
+                        TransferClient, TransferData)
+from utils.decorators import authenticated
+from utils.database import database
+from utils.utils import load_portal_client, get_safe_redirect
+
+
 app = Flask(__name__)
+
+
+@app.route('/signup', methods=['GET'])
+def signup():
+    """Send the user to Globus Auth with signup=1."""
+    return redirect(url_for('authcallback', signup=1))
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    """Send the user to Globus Auth."""
+    return redirect(url_for('authcallback'))
+
+
+@app.route('/logout', methods=['GET'])
+@authenticated
+def logout():
+    """
+    - Revoke the tokens with Globus Auth.
+    - Destroy the session state.
+    - Redirect the user to the Globus Auth logout page.
+    """
+    client = load_portal_client()
+
+    # Revoke the tokens with Globus Auth
+    for token, token_type in (
+            (token_info[ty], ty)
+            # get all of the token info dicts
+            for token_info in session['tokens'].values()
+            # cross product with the set of token types
+            for ty in ('access_token', 'refresh_token')
+            # only where the relevant token is actually present
+            if token_info[ty] is not None):
+        client.oauth2_revoke_token(
+            token, body_params={'token_type_hint': token_type})
+
+    # Destroy the session state
+    session.clear()
+
+    redirect_uri = url_for('home', _external=True)
+
+    ga_logout_url = []
+    ga_logout_url.append(app.config['GLOBUS_AUTH_LOGOUT_URI'])
+    ga_logout_url.append('?client={}'.format(app.config['PORTAL_CLIENT_ID']))
+    ga_logout_url.append('&redirect_uri={}'.format(redirect_uri))
+    ga_logout_url.append('&redirect_name=Globus Sample Data Portal')
+
+    # Redirect the user to the Globus Auth logout page
+    return redirect(''.join(ga_logout_url))
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+@authenticated
+def profile():
+    """User profile information. Assocated with a Globus Auth identity."""
+    if request.method == 'GET':
+        identity_id = session.get('primary_identity')
+        profile = database.load_profile(identity_id)
+
+        if profile:
+            name, email, institution = profile
+
+            session['name'] = name
+            session['email'] = email
+            session['institution'] = institution
+        else:
+            flash(
+                'Please complete any missing profile fields and press Save.')
+
+        if request.args.get('next'):
+            session['next'] = get_safe_redirect()
+
+        return render_template('profile.jinja2')
+    elif request.method == 'POST':
+        name = session['name'] = request.form['name']
+        email = session['email'] = request.form['email']
+        institution = session['institution'] = request.form['institution']
+
+        database.save_profile(identity_id=session['primary_identity'],
+                              name=name,
+                              email=email,
+                              institution=institution)
+
+        flash('Thank you! Your profile has been successfully updated.')
+
+        if 'next' in session:
+            redirect_to = session['next']
+            session.pop('next')
+        else:
+            redirect_to = url_for('profile')
+
+        return redirect(redirect_to)
+
+
+@app.route('/authcallback', methods=['GET'])
+def authcallback():
+    """Handles the interaction with Globus Auth."""
+    # If we're coming back from Globus Auth in an error state, the error
+    # will be in the "error" query string parameter.
+    if 'error' in request.args:
+        flash("You could not be logged into the portal: " +
+              request.args.get('error_description', request.args['error']))
+        return redirect(url_for('home'))
+
+    # Set up our Globus Auth/OAuth2 state
+    redirect_uri = url_for('authcallback', _external=True)
+
+    client = load_portal_client()
+    client.oauth2_start_flow(
+        redirect_uri,
+        refresh_tokens=True,
+        requested_scopes=app.config['USER_SCOPES']
+    )
+
+    # If there's no "code" query string parameter, we're in this route
+    # starting a Globus Auth login flow.
+    if 'code' not in request.args:
+        additional_authorize_params = (
+            {'signup': 1} if request.args.get('signup') else {})
+
+        auth_uri = client.oauth2_get_authorize_url(
+            query_params=additional_authorize_params)
+
+        return redirect(auth_uri)
+    else:
+        # If we do have a "code" param, we're coming back from Globus Auth
+        # and can start the process of exchanging an auth code for a token.
+        code = request.args.get('code')
+        tokens = client.oauth2_exchange_code_for_tokens(code)
+
+        id_token = tokens.decode_id_token()
+        session.update(
+            tokens=tokens.by_resource_server,
+            is_authenticated=True,
+            name=id_token.get('name'),
+            email=id_token.get('email'),
+            institution=id_token.get('organization'),
+            primary_username=id_token.get('preferred_username'),
+            primary_identity=id_token.get('sub'),
+        )
+
+        profile = database.load_profile(session['primary_identity'])
+
+        if profile:
+            name, email, institution = profile
+
+            session['name'] = name
+            session['email'] = email
+            session['institution'] = institution
+        else:
+            return redirect(url_for('profile',
+                            next=url_for('transfer')))
+
+        return redirect(url_for('transfer'))
+
 
 @app.route('/api/register_container', methods=['POST'])
 def registerContainer():
