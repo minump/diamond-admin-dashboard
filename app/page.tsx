59d2d6b10ea85f88d5b6444cc0eadb84a8e02e00
Minu Mathew <minumpmathews@gmail.com>
2024-07-01 14:09:26 -0500

    fetchSessionData method used in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,80 @@
+'use client'
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 import { getUsers } from '@/lib/db';
+import { fetchSessionData } from '@/lib/utils';
 
-export default async function DashboardPage({
+
+export default function DashboardPage({
   searchParams
 }: {
   searchParams: { q: string; offset: string };
 }) {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    const sessionData = fetchSessionData();
+    if (sessionData.primaryIdentity) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
   return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+        justifyContent: 'end',
+        alignItems: 'center',
+        height: '100%',
+        flexDirection: 'row'
+        }}
+      >
+      {isAuthenticated ? (
+        <>
+          <div className={'p-4 text-green-500'}>
+            You are logged in
+          </div>
+          <div
+            className={
+              'border-2 rounded-xl p-2 hover:bg-blue-800 hover:text-white cursor-pointer'
+            }
+          >
+            <Link href={'logout'}> Logout</Link>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={'p-4 text-red-500'}>
+            Please sign in before using the app
+          </div>
+          <div
+            className={
+              'border-2 rounded-xl p-2 hover:bg-blue-800 hover:text-white cursor-pointer'
+            }
+          >
+            <Link href={'login'}> Sign In With Globus</Link>
+          </div>
+        </>
+      )}
+    
+    </div>
     <main className="flex flex-1 flex-col p-4 md:p-6">
+      
       <div className="flex items-center mb-8">
-        <h1 className="font-semibold text-lg md:text-2xl">Users</h1>
+        <h1 className="font-semibold text-lg md:text-2xl">Dashboard</h1>
       </div>
       <div className="w-full mb-4">
         {/* <Search value={searchParams.q} /> */}
         <h1>Dashboard</h1>
       </div>
       <div className="w-full mb-4">
-        <h2> You have been authenticated!</h2>
+        <h2> {isAuthenticated ? 'You have been authenticated!' : 'You are not authenticated'}</h2>
       </div>
       {/* <UsersTable users={users} offset={newOffset} /> */}
       {/* <h2> I want to show user's recent jobs information</h2> */}
     </main>
+    </>
+
   );
 }
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
+import { fetchSessionData } from '@/lib/utils';
 
 export default function SignInPage() {
+  const sessionData = fetchSessionData();
+
   return (
     <div
       style={{
@@ -8,19 +11,28 @@ export default function SignInPage() {
         justifyContent: 'start',
         alignItems: 'center',
         height: '100%',
-        flexDirection: 'column'
+        flexDirection: 'row'
       }}
     >
-      <div className={'p-4 text-red-500'}>
-        Please sign in before using the app
-      </div>
-      <div
-        className={
-          'border-2 rounded-xl p-2 hover:bg-blue-800 hover:text-white cursor-pointer'
-        }
-      >
-        <Link href={'login'}> Sign In With Globus</Link>
-      </div>
+      {sessionData.primaryIdentity.length > 0 ? (
+        <div className={'p-4 text-green-500'}>
+          You are logged in
+        </div>
+      ) : (
+        <>
+          <div className={'p-4 text-red-500'}>
+            Please sign in before using the app
+          </div>
+          <div
+            className={
+              'border-2 rounded-xl p-2 hover:bg-blue-800 hover:text-white cursor-pointer'
+            }
+          >
+            <Link href={'login'}> Sign In With Globus</Link>
+          </div>
+        </>
+      )}
+    
     </div>
   );
 }
